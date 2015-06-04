8b5fd2bfb409655565148244c6f7bb86e9ececee
Alejolazzati <ale.lazzati@gmail.com>
2015-06-05 00:30:25 +0200

    tablas

---
+++ creandoTablas.sql
@@ -0,0 +1,213 @@
+use GD1C2015;
+create table Usuario
+(Id_usuario int  identity(1,1) primary key ,
+Useranme Varchar(30) not null,
+Contraseña varchar(30) not null,
+Fecha_creacion date default GETDATE(),
+Ultima_modificacion date not null default GETDATE(), 
+Pregunta_secreta varchar(50) not null,
+Respuesta varchar(50) not null
+);
+
+create table Pais
+(Id_Pais int primary key,
+Descripcion varchar(20) not null
+);
+create table Tipo_DNI(
+Id_DNI int primary key,
+Descripcion Varchar(20) not null 
+);
+
+Create table Localidad (
+Id_localidad int identity(1,1) primary key,
+Descripcion varchar(30) not null,
+);
+
+Create Table Cliente(
+Id_cliente int identity(1,1) primary key,
+Cod_usuario int not null,
+Nombre varchar (30) not null,
+Apellido varchar(30) not null,
+Tipo_documento int not null,
+Numero_documento int not null,
+Mail varchar(30) not null,
+Cod_pais int not null,
+Localidad int not null,
+Calle varchar(30) not null,
+Numero int not null,
+Piso smallint,
+Depto char,
+Fecha_nacimiento date not null,
+unique (Tipo_documento,Numero_Documento),
+unique (mail),
+foreign key (Cod_pais) references Pais(Id_pais),
+foreign key (Cod_usuario) references Usuario(Id_usuario),
+foreign key (Localidad) references Localidad(Id_localidad)
+);
+
+Create table Intentos_login(
+	Id_login int identity(1,1) primary key,
+	Codigo_usuario int not null,
+	Es_correcto bit not null,
+	foreign key (Codigo_usuario) references Usuario(Id_usuario)
+); 
+
+Create table Intentos_fallidos(
+	Id_fallido int identity(1,1) primary key,
+	Cod_login int not null,
+	foreign key (Cod_login) references Intentos_login(Id_login)
+);
+
+Create table Estado_rol(
+	Id_estado int identity(1,1) primary key,
+	Descripcion varchar(20) not null
+);
+
+Create table Rol(
+	Id_rol int identity(1,1) primary key,
+	Nombre_rol varchar(30) not null,
+	Cod_estado int not null,
+	foreign key (Cod_estado) references Estado_rol(Id_estado),
+);
+Create table Usuario_rol(
+	Cod_usuario int not null,
+	Cod_rol int not null,
+	Foreign key (Cod_usuario) references Usuario(Id_usuario),
+	Foreign key (Cod_rol) references Rol(Id_rol)
+);
+
+Create table Funcionalidad(
+	Id_funcionalidad int primary key,
+	Descripcion varchar(30) not null
+);
+
+Create table Rol_funcionalidad(
+	Cod_rol int not null,
+	Cod_funcionalidad int not null
+	foreign key (Cod_rol) references Rol(Id_rol),
+	foreign key (Cod_funcionalidad) references Funcionalidad(Id_funcionalidad)
+);	
+
+create table Categoria(
+	Id_categoria int identity(1,1) primary key,
+	Descripcion varchar(30) not null,
+	Costo float not null
+);
+
+create table Moneda(
+	Id_moneda int identity(1,1) primary key,
+	Descripcion varchar(20) not null,
+	Conversion float not null default 1	
+);
+
+create table Estado_cuenta(
+	Id_estado int identity(1,1) primary key,
+	Descripcion varchar(20) not null
+);
+
+create table Cuenta(
+	Num_cuenta int primary key,
+	Fecha_apertura date not null default getdate(),
+	Codigo_pais int not null,
+	Codigo_moneda int not null,
+	Codigo_categoria int not null,
+	Codigo_cliente int not null,
+	Codigo_estado int not null,
+	Saldo float not null default 0,
+	foreign key (Codigo_pais) references Pais(Id_pais),
+	foreign key (Codigo_moneda) references Moneda(Id_moneda),
+	foreign key (Codigo_categoria) references Categoria(Id_categoria),
+	foreign key (Codigo_cliente) references Cliente(Id_cliente),
+	foreign key (Codigo_estado) references Estado_cuenta(Id_estado)
+);	
+
+Create table Estado_transaccion(
+	Id_estado int identity(1,1) primary key,
+	Descripcion varchar(30) not null,
+);
+
+Create table Transacciones(
+	Id_transaccion int identity(1,1) primary key,
+	Cod_estado int not null,
+	Costo float not null,
+	Fecha date not null default getdate(),
+	foreign key (Cod_estado) references Estado_transaccion(Id_estado)
+);
+
+create table Tipo_modificacion(
+	Id_tipo int identity(1,1) primary key,
+	Descripcion varchar(30) not null,
+);
+
+create table Modificacion_cuenta(
+	Id_modificacion int identity(1,1) primary key,
+	Cod_tipo int not null,
+	Cod_cuenta int not null,
+	Cod_transaccion int not null,
+	foreign key (Cod_tipo) references Tipo_modificacion(Id_tipo),
+	foreign key (Cod_cuenta) references Cuenta(Num_cuenta),
+	foreign key (Cod_transaccion) references Transacciones(Id_transaccion)
+);
+
+create table Transferencias(
+	Id_transferencia int identity(1,1) primary key,
+	Importe float not null,
+	Cod_cuenta_origen int not null,
+	Cod_cuenta_destino int not null,
+	Cod_transaccion int not null,
+	foreign key (Cod_cuenta_origen) references Cuenta(Num_cuenta),
+	foreign key (Cod_cuenta_destino) references Cuenta(Num_cuenta),
+	foreign key (Cod_transaccion) references Transacciones(Id_transaccion)
+);
+	
+create table Bancos(
+	Id_banco int primary key,
+	Nombre_banco varchar(30) not null,
+	Cod_pais int not null,
+	foreign key (Cod_pais) references Pais(Id_pais)
+);
+
+create table Tarjetas_credito(
+	Num_tarjeta int primary key,
+	Cod_cliente int not null,
+	Cod_emisor int not null,
+	Fecha_emision date not null,
+	Fecha_vencimiento date not null,
+	Cod_seguridad int,
+	foreign key (Cod_cliente) references Cliente(Id_cliente),
+	foreign key (Cod_emisor) references Bancos(Id_banco)
+);
+
+create table Depositos(
+	Id_deposito int primary key,
+	Cod_cuenta int not null,
+	Cod_moneda int not null,
+	Importe float not null,
+	Cod_TC int not null,
+	check(Importe>1),
+	foreign key (Cod_cuenta) references Cuenta(Num_cuenta),
+	foreign key (Cod_moneda) references Moneda(Id_moneda),
+	foreign key (Cod_TC) references Tarjetas_credito(Num_tarjeta)
+);
+
+create table Cheque(
+	Id_cheque int identity(1,1) primary key,
+	Num_cheque int not null,
+	Cod_banco int not null,
+	Cod_cliente int not null,
+	Fecha date not null default getdate(),
+	
+	foreign key (Cod_banco) references Bancos(Id_banco),
+	foreign key (Cod_cliente) references Cliente(Id_cliente)
+);
+
+create table Retiros (
+	Id_retiro int primary key,
+	Cod_cuenta int not null,
+	Cod_cheque int not null,
+	foreign key (Cod_cuenta) references Cuenta(Num_cuenta),
+	Foreign key (Cod_cheque) references Cheque(Id_cheque)
+);
+	
+	
+
