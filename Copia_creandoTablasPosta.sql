07e6367866a9aa085e666ddeb99ff9733df5fe38
Alejolazzati <ale.lazzati@gmail.com>
2015-06-13 06:55:11 +0200

    va

--- Copia_creandoTablasPosta.sql
+++ Copia_creandoTablasPosta.sql
@@ -229,6 +229,252 @@ add Cod_moneda int not null;
 Alter table Cheque 
 add foreign key (Cod_moneda) references Moneda(Id_moneda);
 
+
+
+
+Create trigger actualizarSaldosPorTransferencia 
+	on Transferencias
+	for insert, update, delete
+	as
+	Begin transaction 
+	if ((select count (*) from inserted)>0)
+	begin
+	Declare cursor_deInsertados Cursor
+	for (select Cod_cuenta_origen,Cod_cuenta_destino,sum(Importe),Moneda.Conversion From inserted,Moneda
+	where inserted.Cod_moneda=Moneda.Id_moneda
+	group by Cod_cuenta_origen,Cod_cuenta_destino,Moneda.Conversion)
+	Declare @cuentaOrigen int
+	Declare @cuentaDestino int
+	Declare @sumaDeImporte int	
+	Declare @conversion int
+	Declare @ConversionFinal int
+	open cursor_deInsertados
+	fetch next from cursor_deInsertados into @cuentaOrigen,@cuentaDestino,@sumaDeImporte,@conversion
+	while(@@FETCH_STATUS=0)
+	begin
+	select @ConversionFinal=(Select Moneda.Conversion from Moneda,Cuenta
+	where Moneda.Id_moneda=Cuenta.Codigo_moneda and Cuenta.Num_Cuenta=@cuentaDestino)
+	Update Cuenta set Saldo=Saldo+@sumaDeImporte*@conversion/@ConversionFinal where Num_cuenta=@cuentaDestino
+	select @ConversionFinal=(Select Moneda.Conversion from Moneda,Cuenta
+	where Moneda.Id_moneda=Cuenta.Codigo_moneda and Cuenta.Num_Cuenta=@cuentaDestino)
+	Update Cuenta set Saldo=Saldo-@sumaDeImporte*@conversion/@ConversionFinal where Num_cuenta=@cuentaOrigen
+	fetch next from cursor_deInsertados into @cuentaOrigen,@cuentaDestino,@sumaDeImporte,@conversion
+	end
+	close cursor_deInsertados
+	deallocate cursor_deInsertados
+	end
+	
+	if ((select count (*) from deleted)>0)
+	begin
+	Declare cursor_deBorrados Cursor
+	for (select Cod_cuenta_origen,Cod_cuenta_destino,sum(Importe),Moneda.Conversion From deleted,Moneda
+	group by Cod_cuenta_origen,Cod_cuenta_destino,Moneda.Conversion)
+		
+	
+	open cursor_deBorrados
+	fetch next from cursor_deBorrados into @cuentaOrigen,@cuentaDestino,@sumaDeImporte,@conversion
+	while(@@FETCH_STATUS=0)
+	begin
+	select @ConversionFinal=(Select Moneda.Conversion from Moneda,Cuenta
+	where Moneda.Id_moneda=Cuenta.Codigo_moneda and Cuenta.Num_Cuenta=@cuentaDestino)
+	Update Cuenta set Saldo=Saldo-@sumaDeImporte*@conversion/@ConversionFinal where Num_cuenta=@cuentaDestino
+	select @ConversionFinal=(Select Moneda.Conversion from Moneda,Cuenta
+	where Moneda.Id_moneda=Cuenta.Codigo_moneda and Cuenta.Num_Cuenta=@cuentaOrigen)
+	Update Cuenta set Saldo=Saldo+@sumaDeImporte*@conversion/@ConversionFinal where Num_cuenta=@cuentaOrigen
+	fetch next from cursor_deBorrados into @cuentaOrigen,@cuentaDestino,@sumaDeImporte,@conversion
+	end
+	close cursor_deBorrados
+	deallocate cursor_deBorrados
+	end
+	
+	commit;
+	go
+	
+	create trigger ActualizarSaldosPorDeposito
+on Depositos 
+for insert, update, delete
+	as
+	Begin transaction 
+	if ((select count (*) from inserted)>0)
+	begin
+	Declare cursor_deInsertados Cursor
+	for (select Cod_cuenta,sum(Importe),Moneda.Conversion From inserted,Moneda
+	where inserted.Cod_moneda=Moneda.Id_moneda
+	group by Cod_cuenta,Moneda.Conversion)
+	Declare @cuenta int
+	Declare @sumaDeImporte int	
+	Declare @conversion int
+	Declare @ConversionFinal int
+	open cursor_deInsertados
+	fetch next from cursor_deInsertados into @cuenta,@sumaDeImporte,@conversion
+	while(@@FETCH_STATUS=0)
+	begin
+	select @ConversionFinal=(Select Moneda.Conversion from Moneda,Cuenta
+	where Moneda.Id_moneda=Cuenta.Codigo_moneda and Cuenta.Num_Cuenta=@cuenta)
+	Update Cuenta set Saldo=Saldo+@sumaDeImporte*@conversion/@ConversionFinal where Num_cuenta=@cuenta
+	fetch next from cursor_deInsertados into @cuenta,@sumaDeImporte,@conversion
+	end
+	close cursor_deInsertados
+	deallocate cursor_deInsertados
+	end
+	
+	if ((select count (*) from deleted)>0)
+	begin
+	Declare cursor_deBorrados Cursor
+	for (select Cod_cuenta,sum(Importe),Moneda.Conversion From deleted,Moneda
+	group by Cod_cuenta,Moneda.Conversion)
+		
+	
+	open cursor_deBorrados
+	fetch next from cursor_deBorrados into @cuenta,@sumaDeImporte,@conversion
+	while(@@FETCH_STATUS=0)
+	begin
+	select @ConversionFinal=(Select Moneda.Conversion from Moneda,Cuenta
+	where Moneda.Id_moneda=Cuenta.Codigo_moneda and Cuenta.Num_Cuenta=@cuenta)
+	Update Cuenta set Saldo=Saldo-@sumaDeImporte*@conversion/@ConversionFinal where Num_cuenta=@cuenta
+	fetch next from cursor_deBorrados into @cuenta,@sumaDeImporte,@conversion
+	end
+	close cursor_deBorrados
+	deallocate cursor_deBorrados
+	end
+	
+	commit;
+	go
+		create trigger ActualizarSaldosPorRetiro
+on Retiros 
+for insert, update, delete
+	as
+	Begin transaction 
+	if ((select count (*) from inserted)>0)
+	begin
+	Declare cursor_deInsertados Cursor
+	for (select Cod_cuenta,sum(Cheque.Importe),Moneda.Conversion From Cheque,inserted,Moneda
+	where Cheque.Cod_moneda=Moneda.Id_moneda and Cheque.Id_cheque=inserted.Cod_cheque
+	group by Cod_cuenta,Moneda.Conversion)
+	Declare @cuenta int
+	Declare @sumaDeImporte int	
+	Declare @conversion int
+	Declare @ConversionFinal int
+	open cursor_deInsertados
+	fetch next from cursor_deInsertados into @cuenta,@sumaDeImporte,@conversion
+	while(@@FETCH_STATUS=0)
+	begin
+	select @ConversionFinal=(Select Moneda.Conversion from Moneda,Cuenta
+	where Moneda.Id_moneda=Cuenta.Codigo_moneda and Cuenta.Num_Cuenta=@cuenta)
+	Update Cuenta set Saldo=Saldo-@sumaDeImporte*@conversion/@ConversionFinal where Num_cuenta=@cuenta
+	fetch next from cursor_deInsertados into @cuenta,@sumaDeImporte,@conversion
+	end
+	close cursor_deInsertados
+	deallocate cursor_deInsertados
+	end
+	
+	if ((select count (*) from deleted)>0)
+	begin
+	Declare cursor_deBorrados Cursor
+	for (select Cod_cuenta,sum(Cheque.Importe),Moneda.Conversion From Cheque,deleted,Moneda
+	where Cheque.Cod_moneda=Moneda.Id_moneda and Cheque.Id_cheque=deleted.Cod_cheque
+	group by Cod_cuenta,Moneda.Conversion)
+	
+	open cursor_deBorrados
+	fetch next from cursor_deBorrados into @cuenta,@sumaDeImporte,@conversion
+	while(@@FETCH_STATUS=0)
+	begin
+	select @ConversionFinal=(Select Moneda.Conversion from Moneda,Cuenta
+	where Moneda.Id_moneda=Cuenta.Codigo_moneda and Cuenta.Num_Cuenta=@cuenta)
+	Update Cuenta set Saldo=Saldo+@sumaDeImporte*@conversion/@ConversionFinal where Num_cuenta=@cuenta
+	fetch next from cursor_deBorrados into @cuenta,@sumaDeImporte,@conversion
+	end
+	close cursor_deBorrados
+	deallocate cursor_deBorrados
+	end
+	
+	commit;
+	go
+	Create trigger CuaandoSeIngresanLoginsIncorrectos
+on Intentos_login
+for  insert
+as
+Begin transaction
+Declare @num_login int
+Declare @Correcto bit 
+select Id_login,Es_Correcto into #tablaIncorrectos  from inserted
+where Es_Correcto=0
+
+if ((select count(*) from #tablaIncorrectos
+)>0)
+begin
+Declare cursorDeIncorrectos Cursor
+for select * from #tablaIncorrectos
+open cursorDeIncorrectos
+fetch next from cursorDeIncorrectos into @num_login,@Correcto
+while(@@FETCH_STATUS=0)
+begin
+Insert into Intentos_fallidos (Cod_login)values (@num_login)
+fetch next from cursorDeIncorrectos into @num_login,@Correcto
+end
+end
+close cursorDeIncorrectos
+deallocate cursorDeIncorrectos
+drop table #tablaIncorrectos
+commit;
+go
+
+Create trigger CuaandoSeIngresanLoginsCorrectos
+on Intentos_login
+for  insert
+as
+Begin transaction
+Declare @num_login int
+Declare @Correcto bit 
+Declare @cantidad int
+select Id_login,Es_Correcto into #tablaCorrectos  from inserted
+where Es_Correcto=1
+
+if ((select count(*) from #tablaCorrectos
+)>0)
+begin
+Declare cursorDeIncorrectos Cursor
+for select * from #tablaCorrectos
+open cursorDeIncorrectos
+fetch next from cursorDeIncorrectos into @num_login,@Correcto
+while(@@FETCH_STATUS=0)
+begin
+Delete from Intentos_fallidos where Cod_login=@num_login
+fetch next from cursorDeIncorrectos into @num_login,@Correcto
+end
+end
+drop table #tablaCorrectos
+close cursorDeIncorrectos
+deallocate cursorDeIncorrectos
+commit;
+go
+
+Create trigger CuandoSeIngresaUnTercerLoginFallidoSEInhabilita
+on Intentos_fallidos
+for  insert
+as
+Begin transaction
+Declare @num_fallido int
+Declare @Cod_loguin int
+Declare cursorDeFallidos Cursor
+for select * from inserted
+open cursorDeIncorrectos
+fetch next from cursorDeFallidos into @num_fallido,@Cod_loguin
+while(@@FETCH_STATUS=0)
+begin
+if ((select count(*) from Intentos_fallidos,Usuario,Intentos_loguin
+where Intentos_fallidos.Cod_loguin=Intentos_loguin.Id_loguin and Intentos_loguin.Cod_usuario=Usuario.Id_usuario)>2)
+update Usuario
+set Estado="inhabilitado"
+where Id_usuario = (Select Cod_usuario from Intentos_loguin where Id_loguin=@Cod_loguin)
+fetch next from cursorDeFallidos into @num_fallido,@Cod_loguin
+
+end
+close cursorDeFallidos
+deallocate cursorDeFallidos
+commit;
+go
+
 -----------------------------------------
 alter table Pais alter column descripcion varchar(50);
 
