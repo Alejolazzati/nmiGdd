40521b6574284da3849d429256255926216cdd87
fede22 <fede192@hotmail.com>
2015-06-28 15:36:58 -0300

    Agregada coneccion.cs para conectar con la base (tiene que llamarse sqlserver2008 el motor y GD1C2015 la base).

---
+++ PagoElectronico/PagoElectronico/Coneccion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace PagoElectronico
+{
+    class Coneccion
+    {
+        public static SqlConnection conectarBase()
+        {
+      
+        string conectar = @"Data source = john-vaio\sqlserver2008; Initial Catalog = GD1C2015; Trusted_Connection = YES";
+        SqlConnection cone = new SqlConnection(conectar);
+        cone.Open();
+        return cone;
+        }
+
+    }
+}
