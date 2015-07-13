7d03fdee5bff6278339afcf70c96c20a4fdbf374
fede22 <fede192@hotmail.com>
2015-07-13 15:49:20 -0300

    Agregada fecha de sistema al archivo de configuración,eliminado harcodeo en coneccion.cs

--- PagoElectronico/PagoElectronico/Coneccion.cs
+++ PagoElectronico/PagoElectronico/Coneccion.cs
@@ -8,30 +8,33 @@ namespace PagoElectronico
 {
     class Coneccion
     {
-        
+
         static System.Data.SqlClient.SqlConnection con = conectarBase();
         static SqlCommand com = comando();
+
         public static SqlConnection conectarBase()
-        {string conectar = @"Data source = localhost\SQLSERVER2008; Initial Catalog = GD1C2015; Trusted_Connection = YES;User=gd;Password=gd2015";
-        SqlConnection cone = new SqlConnection(conectar);
-        cone.Open();
-        return cone;
+        {
+            //string fechaDelSistema = Properties.Settings.Default.fechaDelSistema;
+            SqlConnection cone = new SqlConnection(PagoElectronico.Properties.Settings.Default.GD1C2015ConnectionString);
+            cone.Open();
+            return cone;
         }
         public static SqlCommand getComando()
-        {    
-                       
+        {
+
             return com;
         }
         public static SqlCommand comando()
-        {SqlCommand comand=new SqlCommand();
-        comand.Connection = con;
+        {
+            SqlCommand comand = new SqlCommand();
+            comand.Connection = con;
             return comand;
         }
-       /* public static SqlDataAdapter getAdapter()
-        {
-            SqlDataAdapter adapter = new SqlDataAdapter(con);
+        /* public static SqlDataAdapter getAdapter()
+         {
+             SqlDataAdapter adapter = new SqlDataAdapter(con);
         
-        }*/
+         }*/
 
 
 
