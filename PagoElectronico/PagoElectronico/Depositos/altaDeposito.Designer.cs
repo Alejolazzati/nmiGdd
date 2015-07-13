2dffba94cb59196bfbfbb12903f81783a5b2b5bf
fede22 <fede192@hotmail.com>
2015-07-13 19:36:54 -0300

    Agregada funcionalidad a la interfaz de altaDeposito, falta el sql para cuando se aprieta el boton realizar depósito

--- PagoElectronico/PagoElectronico/Depositos/altaDeposito.Designer.cs
+++ PagoElectronico/PagoElectronico/Depositos/altaDeposito.Designer.cs
@@ -29,129 +29,175 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 34);
+            this.label1.Location = new System.Drawing.Point(21, 195);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(42, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Importe";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(124, 34);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 67);
+            this.label2.Location = new System.Drawing.Point(18, 227);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tipo moneda";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(124, 64);
+            this.comboBox1.Location = new System.Drawing.Point(124, 227);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 105);
+            this.label3.Location = new System.Drawing.Point(21, 267);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Tarjeta de credito";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(124, 105);
+            this.listBox1.Location = new System.Drawing.Point(124, 267);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(268, 95);
             this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 218);
+            this.label4.Location = new System.Drawing.Point(18, 375);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(97, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Fecha del depósito";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(124, 215);
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(124, 375);
             this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(144, 20);
             this.textBox2.TabIndex = 7;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(278, 215);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Seleccionar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(409, 278);
+            this.button2.Location = new System.Drawing.Point(409, 428);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(118, 32);
             this.button2.TabIndex = 9;
             this.button2.Text = "Realizar depósito";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // textBox3
             // 
-            this.button3.Location = new System.Drawing.Point(422, 105);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(105, 23);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "Seleccionar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.textBox3.Location = new System.Drawing.Point(396, 42);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(131, 20);
+            this.textBox3.TabIndex = 46;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(306, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 45;
+            this.label5.Text = "Saldo disponible";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 13);
+            this.label6.TabIndex = 44;
+            this.label6.Text = "Cuentas disponibles:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(21, 39);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(283, 134);
+            this.listBox2.TabIndex = 43;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 425);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 32);
+            this.button4.TabIndex = 47;
+            this.button4.Text = "Volver atras";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(124, 192);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(121, 20);
+            this.maskedTextBox1.TabIndex = 48;
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
             // 
             // altaDeposito
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 328);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(534, 469);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "altaDeposito";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.altaDeposito_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,15 +206,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
     }
 }
--- PagoElectronico/PagoElectronico/Depositos/altaDeposito.cs
+++ PagoElectronico/PagoElectronico/Depositos/altaDeposito.cs
@@ -14,6 +14,130 @@ namespace PagoElectronico.Depositos
         public altaDeposito()
         {
             InitializeComponent();
+            System.Data.SqlClient.SqlCommand comando = Coneccion.getComando();
+            comando.CommandText = "Select * from NMI.cuentasPorCliente(" + Program.cliente + ")";
+            System.Data.SqlClient.SqlDataReader reader = comando.ExecuteReader();
+
+            while (reader.Read())
+            {
+                listBox2.Items.Add(reader.GetSqlValue(0));
+                this.Show();
+
+
+            }
+            reader.Dispose();
+
+            comando.CommandText = "select NMI.Moneda.Descripcion from NMI.Moneda";
+            reader = comando.ExecuteReader();
+            while (reader.Read())
+            {
+                comboBox1.Items.Add(reader.GetSqlString(0));
+
+            }
+            this.Show();
+            reader.Dispose();
+
+            comando.CommandText = "Select * from NMI.tarjetasPorCliente(" + Program.cliente + ")";
+            reader = comando.ExecuteReader();
+
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader.GetSqlValue(0));
+                this.Show();
+
+
+            }
+            reader.Dispose();
+
+            maskedTextBox1.Mask = "0999999999999999999999999999";
+
+            textBox2.Text = Properties.Settings.Default.fechaDelSistema; 
+        }
+
+        private void altaDeposito_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String cuenta = listBox2.SelectedItem.ToString();
+            System.Data.SqlClient.SqlCommand comando = Coneccion.getComando();
+            comando.CommandText = "Select NMI.saldoCuenta(" + cuenta + ")";
+            System.Data.SqlClient.SqlDataReader reader = comando.ExecuteReader();
+            reader.Read();
+            textBox3.Text = reader.GetSqlValue(0).ToString();
+            reader.Dispose();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+        private void maskedTextBox1_Validating(object sender, CancelEventArgs e)
+        {
+            e.Cancel = int.Parse(maskedTextBox1.Text) > 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PagoElectronico.Login.Funcionalidades formFuncion = new PagoElectronico.Login.Funcionalidades();
+            formFuncion.Show();
         }
     }
 }
