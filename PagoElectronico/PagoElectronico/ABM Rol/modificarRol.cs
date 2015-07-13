4f184c5f525e8b765bac7e947b31104628446c62
martinnbasilee <martinnbasile@gmail.com>
2015-07-12 22:08:02 -0300

    golis

--- PagoElectronico/PagoElectronico/ABM Rol/modificarRol.cs
+++ PagoElectronico/PagoElectronico/ABM Rol/modificarRol.cs
@@ -12,11 +12,26 @@ namespace PagoElectronico.ABM_Rol
     public partial class modificarRol : Form
     {
         String rol;
+        System.Data.SqlClient.SqlCommand comando = Coneccion.getComando();
+        
         public modificarRol(String unRol)
         {
             InitializeComponent();
             rol = unRol;
             textBox1.Text = rol;
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            
+            comando.CommandText = "select descripcion from NMI.Estado_rol ";
+            System.Data.SqlClient.SqlDataReader reader = comando.ExecuteReader();
+
+            while (reader.Read())
+            {
+                comboBox2.Items.Add(reader.GetSqlString(0));
+            }
+            this.Show();
+
+            reader.Dispose();
+
         }
 
         private void modificarRol_Load(object sender, EventArgs e)
@@ -25,6 +40,11 @@ namespace PagoElectronico.ABM_Rol
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
